b852b0b9b888d19fe27d269b11e9a188a320288f
Yuukago <yuka.ago.work@gmail.com>
2026-03-30 20:11:57 +0900

    deviseを導入し、Userモデルを追加。サインアップ時にユーザー情報（郵便番号・住所・自己紹介）を登録できるようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,21 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  def after_sign_in_path_for(resource_or_scope)
+    "/books"
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    "/users/sign_in"
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:postal_code, :address, :introduction])
+  end
 end
